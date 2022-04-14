8cb1eb40ce8041f80eeee0beeb8b88cc86e3f050
박정우 (Park) <79061791+ParkJ82@users.noreply.github.com>
2022-04-14 13:02:28 -0400

    Update input.js

--- input.js
+++ input.js
@@ -3,6 +3,6 @@ const nameGenerator = document.getElementById("inputtedName");
 buttonClick.addEventListener('click', getName);
 
 function getName() {
-    location.replace("C:/Users/qkrwj/Desktop/Computer%20Science/Chrome%20Extension/daily%20screener/dailyscreener.html")
+    location.replace("dailyscreener.html")
     sessionStorage.setItem("full name", nameGenerator.value);
 };
