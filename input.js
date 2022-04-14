0771d91032cf05ef05bce2ccbdf6825a2a456ba8
박정우 (Park) <79061791+ParkJ82@users.noreply.github.com>
2022-04-14 18:14:40 -0400

    Update input.js

--- input.js
+++ input.js
@@ -4,7 +4,18 @@ const lastName = document.getElementById("lastName");
 buttonClick.addEventListener('click', getName);
 
 function getName() {
-    location.replace("/dailyscreener.html");
-    const fullName = `${firstName.value} ${lastName.value}`
-    sessionStorage.setItem("full name", fullName);
+    if (firstName.value != "" || lastName.value != ""){
+        location.replace("/dailyscreener.html");
+        const fullName = `${firstName.value} ${lastName.value}`
+        sessionStorage.setItem("full name", fullName);
+    }
+    else {
+        if (typeof sessionStorage.getItem("full name") != "undefined") {
+            location.replace("/dailyscreener.html");
+        }
+        else {
+            alert("must input a value");
+        }
+    };
+    
 };
