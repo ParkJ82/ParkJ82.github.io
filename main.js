7efbbec9285396d508374ec76c82826a90204cfe
박정우 (Park) <79061791+ParkJ82@users.noreply.github.com>
2022-04-15 07:45:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ var yourName = localStorage.getItem("full name");
 let today = new Date();
 today = today.toDateString().split(" ");
 today.shift();
+const month = today[0];
+const day = today[1];
+today[0] = day;
+today[1] = month;
 today = today.join(" ");
 
 nameSelector.textContent = yourName;
