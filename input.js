02bdc082cbaebdce18cad7725ad9f0352b13885b
박정우 (Park) <79061791+ParkJ82@users.noreply.github.com>
2022-04-15 10:40:38 -0400

    Update input.js

--- input.js
+++ input.js
@@ -1,25 +1,37 @@
-const buttonClick = document.querySelector(".generateButton");
+const buttonDailyClick = document.querySelector(".generateDailyButton");
+const buttonVaxClick = document.querySelector(".generateVaxButton");
 const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
-buttonClick.addEventListener('click', getName);
+buttonDailyClick.addEventListener('click', function() {getName("Daily");});
+buttonVaxClick.addEventListener('click', function () {getName("Vax");});
+
+
 
 function isEmpty(str) {
     return !str.trim().length;
 }
 
-function getName() {
+function getName(typeOfScreen) {
     if (!isEmpty(firstName.value) || !isEmpty(lastName.value)){
-        location.replace("/dailyscreener.html");
-        const fullName = `${firstName.value} ${lastName.value}`
+        if (typeOfScreen === "Daily") {
+            location.replace("/dailyscreener.html");
+        } else if (typeOfScreen === "Vax") {
+            location.replace("/vaxpass.html");
+        };
+        const fullName = `${firstName.value} ${lastName.value}`;
         localStorage.setItem("full name", fullName);
     }
     else {
         if (typeof localStorage.getItem("full name") != "undefined") {
-            location.replace("/dailyscreener.html");
+            if (typeOfScreen === "Daily") {
+                location.replace("/dailyscreener.html");
+            } else if (typeOfScreen === "Vax") {
+                location.replace("/vaxpass.html");
+            };
         }
         else {
             alert("must input a value");
-        }
+        };
     };
     
 };
